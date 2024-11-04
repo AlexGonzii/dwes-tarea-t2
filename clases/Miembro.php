1234c0c2cc8f5816494da04aefea6be1463799a0
AlexGonzii <150667809+AlexGonzii@users.noreply.github.com>
2024-11-04 10:05:53 +0100

    Modificar método toString

--- clases/Miembro.php
+++ clases/Miembro.php
@@ -38,7 +38,7 @@
         }
 
         public function __toString(){
-            return "Miembro: (ID: " .$this->id. ", Nombre: ". $this->nombre . ", Apellidos: " . $this->apellidos . ", Email:  " .$this->email . ")";
+            return "Nombre: ". $this->nombre . "," . $this->apellidos . ", Email:  " .$this->email;
         }
     }
 ?>
