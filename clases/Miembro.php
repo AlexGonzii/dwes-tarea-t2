91242c5f1f01adf1e24e96ee96c50006eda291ff
AlexGonzii <150667809+AlexGonzii@users.noreply.github.com>
2024-11-04 09:24:58 +0100

    Creación de la clase Miembro con sus métodos y atributos

---
+++ clases/Miembro.php
@@ -0,0 +1,44 @@
+<?php 
+    abstract class Miembro{
+        protected int $id;
+        protected $nombre;
+        protected $apellidos;
+        protected $email;
+
+        public function __construct(int $id,$nombre,$apellidos,$email){
+            $this->id = $id;
+            $this->nombre = $nombre;
+            $this->apellidos = $apellidos;
+            $this->email = $email;
+        }
+
+        public function getId(): int{
+                return $this->id;
+        }
+        public function setId(int $id){
+                $this->id = $id;
+        }
+        public function getNombre(){
+                return $this->nombre;
+        }
+        public function setNombre($nombre){
+                $this->nombre = $nombre;
+        }
+        public function getApellidos(){
+                return $this->apellidos;
+        }
+        public function setApellidos($apellidos){
+                $this->apellidos = $apellidos;
+        }
+        public function getEmail(){
+                return $this->email;
+        }
+        public function setEmail($email){
+                $this->email = $email;
+        }
+
+        public function __tostring(){
+            return "Miembro: (ID: " .$this->id. ", Nombre: ". $this->nombre . ", Apellidos: " . $this->apellidos . ", Email:  " .$this->email . ")";
+        }
+    }
+?>
