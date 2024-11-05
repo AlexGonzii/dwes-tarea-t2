67385f240b4b80ab0808d682370984c60d40a5e3
AlexGonzii <150667809+AlexGonzii@users.noreply.github.com>
2024-11-05 13:39:43 +0100

    Modificación de las etiquetas de html y php

--- index.php
+++ index.php
@@ -33,66 +33,66 @@
     $alumnos[9]->matricularEnAsignatura($asignaturas[0]);
 ?>
 <h2>Alumnos</h2>
-<?php
-echo "<ul>";
-foreach($alumnos as $alumno) {
-    echo "<li>" . $alumno . "</li>";
-    }
-echo "</ul>";
-?>
+<ul>
+<?php foreach($alumnos as $alumno) { ?>
+    <li> 
+      <?= $alumno ?>
+    </li>
+    <?php } ?>
+</ul>
 <h2>Profesores</h2>
-<?php
-echo "<ul>";
-foreach($profesores as $profesor) {
-    echo "<li>" . $profesor . "</li>";
-    }
-echo "</ul>";
-?>
+<ul>
+<?php foreach($profesores as $profesor) { ?>
+    <li>
+       <?= $profesor ?>
+    </li>
+    <?php } ?>
+</ul>
 <h2>Asignaturas</h2>
-<?php
-echo "<ul>";
-foreach($asignaturas as $asignatura) {
-    echo "<li>" . $asignatura . "</li>";
-    }
-echo "</ul>";
-?>
+<ul>
+<?php foreach($asignaturas as $asignatura) { ?>
+    <li> 
+       <?= $asignatura ?> 
+    </li>
+    <?php } ?>
+</ul>
 <h2>Alumnos <= 23</h2>
-<?php
-$filtrarEdad = array_filter($alumnos,function($alumno){
+<?php $filtrarEdad = array_filter($alumnos,function($alumno){
     return $alumno->getEdad() <= 23;
-});
-echo "<ul>";
-foreach($filtrarEdad as $alumno) {
-    echo "<li>" . $alumno . "</li>";
-    }
-echo "</ul>";
-?>
+});?>
+<ul>
+<?php foreach($filtrarEdad as $alumno) { ?>
+    <li>
+        <?= $alumno ?> 
+    </li>
+    <?php } ?>
+</ul>
 <h2>Alumnos con al menos dos asignaturas</h2>
-<?php
-$filtrarAsig = array_filter($alumnos,function($alumno){
+<?php $filtrarAsig = array_filter($alumnos,function($alumno){
     return count($alumno->getAsignaturas()) >= 2;
-});
-echo "<ul>";
-foreach($filtrarAsig as $alumno) {
-    echo "<li>" . $alumno . "</li>";
-    }
-echo "</ul>";
-?>
+});?>
+<ul>
+<?php foreach($filtrarAsig as $alumno) { ?>
+    <li>
+        <?= $alumno ?> 
+    </li>
+    <?php } ?>
+</ul>
 <h2>Asignaturas con algún alumno matriculado</h2>
-<?php
-$asignaturasConAlumnos = array_filter($asignaturas, function($asignatura) use ($alumnos) {
+<?php $asignaturasConAlumnos = array_filter($asignaturas, function($asignatura) use ($alumnos) {
     foreach ($alumnos as $alumno) {
         if (in_array($asignatura, $alumno->getAsignaturas())) {
             return true;
         }
     }
     return false;
-});
-echo "<ul>";
-foreach ($asignaturasConAlumnos as $asignatura) {
-    echo "<li>" . $asignatura . "</li>";
-}
-echo "</ul>";
-?>
+});?>
+<ul>
+<?php foreach ($asignaturasConAlumnos as $asignatura) { ?>
+    <li>
+       <?= $asignatura ?> 
+    </li>
+    <?php } ?>
+</ul>
 </body>
 </html>
